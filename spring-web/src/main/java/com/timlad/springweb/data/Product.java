24f812878fcb24888cfcd8201fc0fa80b97baac7
Timofey <timofey.ladatko@gmail.ru>
2022-04-21 18:40:43 +0300

    ДЗ к уроку 5

---
+++ spring-web/src/main/java/com/timlad/springweb/data/Product.java
@@ -0,0 +1,27 @@
+package com.timlad.springweb.data;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Accessors(chain = true)
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "coast")
+    private int coast;
+
+}
