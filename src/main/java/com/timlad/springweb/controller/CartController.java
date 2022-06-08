68f530e26ae9a92d77b664b305a4dc5c51097629
Timofey <timofey.ladatko@gmail.ru>
2022-06-09 00:33:51 +0300

    ДЗ к уроку 12

---
+++ src/main/java/com/timlad/springweb/controller/CartController.java
@@ -0,0 +1,43 @@
+package com.timlad.springweb.controller;
+
+import com.timlad.springweb.converters.ProductConverter;
+import com.timlad.springweb.dto.ProductDto;
+import com.timlad.springweb.entities.Product;
+import com.timlad.springweb.exeptions.ResourceNotFoundException;
+import com.timlad.springweb.service.CartService;
+import com.timlad.springweb.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/carts")
+public class CartController {
+    private final CartService cartService;
+    private final ProductService productService;
+    private final ProductConverter productConverter;
+
+    @GetMapping
+    public List<ProductDto> loadCart() {
+     return cartService.loadCart();
+    }
+
+    @GetMapping("/add/{id}")
+    public void addProductToCart(@PathVariable Long id) {
+        Product product = productService.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Product not found, id: " + id));
+        cartService.addProductToCart(productConverter.entityToDto(product));
+    }
+
+    @GetMapping("/delete/{id}")
+    public void deleteProductFromCart(@PathVariable Long id) {
+        cartService.deleteProductFromCart(id);
+    }
+
+    @PutMapping
+    public void changeAmountProduct(@RequestParam Long productId, @RequestParam int delta) {
+        cartService.changeAmountProduct(productId, delta);
+    }
+}
---
+++ src/main/resources/static/index.js
@@ -0,0 +1,125 @@
+angular.module('app', ['ngStorage']).controller('indexController', function ($scope, $http, $rootScope, $localStorage) {
+    const contextPath = 'http://localhost:8189/app/api/v1';
+
+    if ($localStorage.springWebUser) {
+        $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.springWebUser.token;
+    }
+
+    $scope.loadProducts = function (numberPage) {
+
+        if (isNaN(numberPage)){numberPage = 1;}
+
+        $http({
+            url: contextPath + '/products',
+            method: 'GET',
+            params: {
+                title_part: $scope.filter ? $scope.filter.title_part : null,
+                min_price: $scope.filter ? $scope.filter.min_price : null,
+                max_price: $scope.filter ? $scope.filter.max_price : null,
+                number_page: numberPage
+
+            }
+        }).then(function (response) {
+                $scope.ProductsPage = response.data;
+            });
+    };
+
+    $scope.tryToAuth = function () {
+        $http.post('http://localhost:8189/app/auth', $scope.user)
+            .then(function successCallback(response) {
+                if (response.data.token) {
+                    $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
+                    $localStorage.springWebUser = {username: $scope.user.username, token: response.data.token};
+
+                    $scope.user.username = null;
+                    $scope.user.password = null;
+                }
+            }, function errorCallback(response) {
+            });
+    };
+
+    $scope.tryToLogout = function () {
+        $scope.clearUser();
+        if ($scope.user.username) {
+            $scope.user.username = null;
+        }
+        if ($scope.user.password) {
+            $scope.user.password = null;
+        }
+    };
+
+    $scope.clearUser = function () {
+        delete $localStorage.springWebUser;
+        $http.defaults.headers.common.Authorization = '';
+    };
+
+    $rootScope.isUserLoggedIn = function () {
+        if ($localStorage.springWebUser) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    $scope.loadProductsNotFilter = function (numberPage) {
+
+        if (isNaN(numberPage)){numberPage = 1;}
+
+        $http({
+            url: contextPath + '/products',
+            method: 'GET',
+            params: {
+                number_page: numberPage
+            }
+        }).then(function (response) {
+                $scope.ProductsPage = response.data;
+            });
+    };
+
+    $scope.addProductToCart = function (productId) {
+        $http.get(contextPath + '/carts/add/' + productId)
+        .then(function (response) {
+            $scope.ProductAmount = response.data;
+            $scope.loadCart();
+        });
+    };
+
+    $scope.deleteProductFromCart = function (productId) {
+        $http.get(contextPath + '/carts/delete/' + productId)
+            .then(function (response) {
+                $scope.loadCart();
+            });
+    }
+
+    $scope.loadCart = function () {
+        $http.get(contextPath + '/carts')
+            .then(function (response) {
+                $scope.Cart = response.data;
+            });
+    };
+
+    $scope.changeAmountProduct = function (productId, delta) {
+        $http({
+            url: contextPath + '/carts',
+            method: 'PUT',
+            params: {
+                productId: productId,
+                delta: delta
+            }
+        }).then(function (response) {
+            $scope.loadCart();
+        });
+    }
+
+    $scope.showCurrentUserInfo = function () {
+        $http.get('http://localhost:8189/app/api/v1/profile')
+            .then(function successCallback(response) {
+                alert('MY NAME IS: ' + response.data.username);
+            }, function errorCallback(response) {
+                alert('UNAUTHORIZED');
+            });
+    }
+
+    $scope.loadCart();
+    $scope.loadProducts();
+});
